6d9abace57b7aeb3b57ce7d4ef0edcb6671f67bd
BrunoSlamek <61333566+BrunoSlamek@users.noreply.github.com>
2020-11-01 19:46:31 -0300

    add code

---
+++ Python/ex_certification.py
@@ -0,0 +1,19 @@
+"""
+Solução primeiro exercicio da certificação python basico do hackerrank.
+"""
+
+class Car:
+  def __init__(self, max_speed, speed_unit):
+    self.max_speed = max_speed
+    self.speed_unit = speed_unit
+  def __str__(self):
+    my_string = "Car with the maximum speed of {} {}"
+    return my_string.format(self.max_speed, self.speed_unit)
+
+class Boat:
+  def __init__(self, max_speed):
+    self.max_speed = max_speed
+
+  def __str__(self):
+    my_string = "Boat with the maximum speed of {} knots"
+    return my_string.format(self.max_speed)
